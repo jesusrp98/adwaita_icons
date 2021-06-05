7eccdff2a73ea2132ecb4fc887d8450883723c13
jesusrp98 <jesusrope98@outlook.com>
2021-06-05 16:36:37 +0200

    Add code documentation

--- lib/src/adwaita_icon.dart
+++ lib/src/adwaita_icon.dart
@@ -1,13 +1,52 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-const defaultIconSize = 24.0;
-const defaultIconColor = Color(0xFF000000);
-
+/// This widget uses the [flutter_svg] package to render 'adwaita' style icons,
+/// provided by the GNOME team. They're located inside the 'assets' folder.
 class AdwaitaIcon extends StatelessWidget {
+  /// Asset path of the specific icon. The file should be an 'svg'.
+  /// The list to all available icons is inside the `icon_library.dart` file.
   final String asset;
+
+  /// The size of the icon in logical pixels.
+  ///
+  /// Icons occupy a square with width and height equal to size.
+  ///
+  /// Defaults to the current [IconTheme] size, if any. If there is no
+  /// [IconTheme], or it does not specify an explicit size, then it defaults to
+  /// 24.0.
+  ///
+  /// If this [Icon] is being placed inside an [IconButton], then use
+  /// [IconButton.iconSize] instead, so that the [IconButton] can make the splash
+  /// area the appropriate size as well. The [IconButton] uses an [IconTheme] to
+  /// pass down the size to the [Icon].
   final double? size;
+
+  /// The color to use when drawing the icon.
+  ///
+  /// Defaults to the current [IconTheme] color, if any.
+  ///
+  /// The given color will be adjusted by the opacity of the current
+  /// [IconTheme], if any.
+  ///
+  ///
+  /// In material apps, if there is a [Theme] without any [IconTheme]s
+  /// specified, icon colors default to white if the theme is dark
+  /// and black if the theme is light.
+  ///
+  /// If no [IconTheme] and no [Theme] is specified, icons will default to black.
+  ///
+  /// See [Theme] to set the current theme and [ThemeData.brightness]
+  /// for setting the current theme's brightness.
   final Color? color;
+
+  /// Semantic label for the icon.
+  ///
+  /// Announced in accessibility modes (e.g TalkBack/VoiceOver).
+  /// This label does not show in the UI.
+  ///
+  ///  * [SemanticsProperties.label], which is set to [semanticLabel] in the
+  ///    underlying	 [Semantics] widget.
   final String? semanticLabel;
 
   const AdwaitaIcon(
@@ -20,12 +59,13 @@ class AdwaitaIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final iconTheme = IconTheme.of(context);
     return SvgPicture.asset(
       asset,
       package: 'adwaita_icons',
-      width: size ?? defaultIconSize,
-      height: size ?? defaultIconSize,
-      color: color ?? defaultIconColor,
+      width: size ?? iconTheme.size,
+      height: size ?? iconTheme.size,
+      color: color ?? iconTheme.color,
       semanticsLabel: semanticLabel,
     );
   }
--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# Location of the folder where all 'adwaita' icons reside.
 ICONS_FOLDER="../assets/icons"
+
+# Output file where all the paths for the icons reside. This is used to
+# build an 'icon widget' within Flutter, using the 'flutter_svg' package.
 LIBRARY_OUTPUT="../lib/src/icon_library.dart"
+
+# Output file where a list containing all icon paths will be placed.
+# This list will be used for the 'example' project available inside this package.
 LIST_OUTPUT="../example/lib/icon_library.dart"
 
+# First we print file headers for each output
 echo -e "class AdwaitaIcons {const AdwaitaIcons._();\n" >$LIBRARY_OUTPUT
 echo "const adwaitaIconPaths = [" >$LIST_OUTPUT
 
@@ -17,14 +25,14 @@ for icon in "$ICONS_FOLDER"/*; do
     fi
     final_icon_name=$(echo "$icon_name" | tr '-' '_')
 
-    # library
+    # Print each icon to the 'library' file
     echo -n "static const " >>$LIBRARY_OUTPUT
     echo -n $final_icon_name >>$LIBRARY_OUTPUT
     echo -n "=\"" >>$LIBRARY_OUTPUT
     echo -n $(realpath --relative-to=.. "$icon") >>$LIBRARY_OUTPUT
     echo "\";" >>$LIBRARY_OUTPUT
 
-    # list
+    # Print each icon to the 'list' file
     echo -n "\"" >>$LIST_OUTPUT
     echo -n $(realpath --relative-to=.. "$icon") >>$LIST_OUTPUT
     echo -n "\"" >>$LIST_OUTPUT
@@ -34,5 +42,6 @@ done
 echo "}" >>$LIBRARY_OUTPUT
 echo "];" >>$LIST_OUTPUT
 
+# Lastly, we format both output files using Flutter's tools.
 flutter format $LIBRARY_OUTPUT
 flutter format $LIST_OUTPUT
